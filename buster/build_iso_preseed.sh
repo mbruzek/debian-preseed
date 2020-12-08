cf799e14531056d83185cfaf01ae92a8946d648d
Matthew Bruzek <mbruzek@gmail.com>
2020-12-07 23:31:52 -0600

    Download checksum file to get the iso name.

--- buster/build_iso_preseed.sh
+++ buster/build_iso_preseed.sh
@@ -1,29 +1,34 @@
 #!/usr/bin/env bash
 
-# Build an ISO image of the Debian network installer with a preseed.cfg file.
-
-# The first variable is the optional Debian version.
-DEBIAN_VERSION=${1:-10.7.0}
-DEBIAN_ISO_FILE_NAME=debian-${DEBIAN_VERSION}-amd64-netinst.iso
-DEBIAN_NETINST_URL=https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/${DEBIAN_ISO_FILE_NAME}
-OUTPUT_FILE=preseed-${DEBIAN_ISO_FILE_NAME}
-YEAR_MONTH_DAY=$(date "+%Y-%m-%d")
+# Download an ISO file, extract the contents, copy in preseed.cfg and generate
+# a new ISO image.
 
 # Ensure the prerequisite software is installed.
 apt install -y debconf isolinux syslinux-utils xorriso wget
 
+# Check the syntax of the preseed file.
 if ! debconf-set-selections -c preseed.cfg ; then
   echo "There is an error in the preseed.cfg. Check the syntax of the preconfiguration file."
   exit 1
 fi
 
-# Do not download the image if it has not changed.
-wget --timestamping ${DEBIAN_NETINST_URL}
+# The URL to the SHA512SUMS of the CD directory.
+CHECKSUM_URL=https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/SHA512SUMS
+# Download the checksum file, do not duplicate if unchanged.
+wget --timestamping ${CHECKSUM_URL}
+
+# The name of the ISO file is on the first line second column.
+ISO_FILE_NAME=$(head -n 1 $(basename ${CHECKSUM_URL}) | awk '{print $2}')
+# Construct the URL to the specific ISO file.
+ISO_URL=https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/${ISO_FILE_NAME}
+
+# Download the ISO file, do not duplicate if the image has not changed.
+wget --timestamping ${ISO_URL}
 
 # Create a working directory.
 mkdir -p iso
 # Restore the iso file to the filesystem in the iso directory.
-xorriso -osirrox on -indev ${DEBIAN_ISO_FILE_NAME} -extract / iso/
+xorriso -osirrox on -indev ${ISO_FILE_NAME} -extract / iso/
 
 chmod +w -R iso/install.amd/
 # Uncompress the initrd gzip.
@@ -42,9 +47,13 @@ chmod +w md5sum.txt
 md5sum `find -follow -type f` > md5sum.txt
 popd
 
+# Create the output file name.
+OUTPUT_FILE=preseed-${ISO_FILE_NAME}
+YEAR_MONTH_DAY=$(date "+%Y-%m-%d")
+
 # Recreate the ISO using options to make it bootable on BIOS and EFI systems.
 xorriso -as mkisofs \
-	-r -V "preseed-netinst-${YEAR_MONTH_DAY}" \
+	-r -V "preseed-iso-${YEAR_MONTH_DAY}" \
 	-o ${OUTPUT_FILE} \
 	-cache-inodes \
 	-isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin \
